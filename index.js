c2f569d55b4dbf6ef07441dc802ab5d98d802661
Don_martinez <donmartinez@aluno.unilab.edu.br>
2024-04-13 18:25:25 -0300

    let's

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+app.listen(5000), () => {
+  console.log('Server running on port 5000');
+}
